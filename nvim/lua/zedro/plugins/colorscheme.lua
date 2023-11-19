b70eaaf4517409e02dc2be12f981e2d81b864f35
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-11-19 10:58:07 +0000

    nvim : transparency on

--- nvim/lua/zedro/plugins/colorscheme.lua
+++ nvim/lua/zedro/plugins/colorscheme.lua
@@ -3,7 +3,11 @@ return {
 	priority = 1000, -- Make sure it loads before all other plugins
 	config = function()
 		-- Load colorscheme
-		vim.cmd([[colorscheme dracula]])
+		vim.cmd([[
+			colorscheme dracula
+			highlight Normal guibg=none
+			highlight NonText guibg=none
+		]])
 	end,
 }
 
