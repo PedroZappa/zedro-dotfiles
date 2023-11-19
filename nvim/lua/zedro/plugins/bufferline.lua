2b5855f363c64ad121078f6d0b790f40d8b5fdb1
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-11-19 17:38:30 +0000

    bufferline.lua : confed

--- nvim/lua/zedro/plugins/bufferline.lua
+++ nvim/lua/zedro/plugins/bufferline.lua
@@ -1,11 +1,22 @@
 return {
-  "akinsho/bufferline.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  version = "*",
-  opts = { -- require(bufferline).setup(opts)
-    options = {
-      mode = "tabs",
-      separator_style = "slant",
-    },
-  },
+	"akinsho/bufferline.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	version = "*",
+	opts = { -- require(bufferline).setup(opts)
+	options = {
+		mode = "tabs",
+		buffer_close_icon = '󰅖',
+		modified_icon = '●',
+		close_icon = '',
+		left_trunc_marker = '',
+		right_trunc_marker = '',
+		tab_size = 18,
+		separator_style = "slant",
+		hover = {
+			enabled = true,
+			delay = 200,
+			reveal = {'close'}
+		},
+	},
+},
 }
