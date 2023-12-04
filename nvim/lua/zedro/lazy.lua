2bc9514c01be3dd3a86d57349648848a9ee643ce
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-12-04 20:29:14 +0000

    plugins/edit/ : Created, plugins re-organized

--- nvim/lua/zedro/lazy.lua
+++ nvim/lua/zedro/lazy.lua
@@ -2,24 +2,25 @@
 -- vim.loader.enable() -- cache lua modules (https://github.com/neovim/neovim/pull/22668)
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",     -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 	{ import = 'zedro.plugins' },
+	{ import = 'zedro.plugins.ctags' },
+	{ import = 'zedro.plugins.edit' },
+	{ import = 'zedro.plugins.git' },
 	{ import = 'zedro.plugins.lsp' },
 	{ import = 'zedro.plugins.theme' },
-	{ import = 'zedro.plugins.ctags' },
-	{ import = 'zedro.plugins.git' },
-	}, {
+}, {
 	install = {
 		colorscheme = { "dracula" },
 	},
