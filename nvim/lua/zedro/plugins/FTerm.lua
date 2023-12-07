335246b6bc15eb112cd5519d6091248c9a3cfedb
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-12-07 13:41:54 +0000

    FTterm.lua : floating htop keymap

--- nvim/lua/zedro/plugins/FTerm.lua
+++ nvim/lua/zedro/plugins/FTerm.lua
@@ -15,20 +15,12 @@ return {
 		vim.keymap.set('n', '<A-i>', '<CMD>lua require("FTerm").toggle()<CR>')
 		vim.keymap.set('t', '<A-i>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
 		-- floating xtop
+		local htop = fterm:new({
+			ftype = 'fterm_htop',
+			cmd = 'htop',
+		})
 		vim.keymap.set('n', '<A-t>', function()
-			if os.execute("which btop") then
-				local btop = fterm:new({
-					ftype = 'fterm_btop',
-					cmd = 'btop',
-				})
-				btop:toggle()
-			else
-				local htop = fterm:new({
-					ftype = 'fterm_htop',
-					cmd = 'htop',
-				})
-				htop:toggle()
-			end
+			htop:toggle()
 		end)
 	end,
 }
