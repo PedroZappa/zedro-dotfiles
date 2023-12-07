efbef5bf57a6831e2c8fb913084cabf5836706ae
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-12-07 13:27:21 +0000

    FTerm.lua : basic setup

---
+++ nvim/lua/zedro/plugins/FTerm.lua
@@ -0,0 +1,26 @@
+return {
+	"numToStr/FTerm.nvim",
+	config = function()
+		local fterm = require'FTerm'
+
+		fterm.setup({
+			border = 'double',
+			dimensions  = {
+				height = 0.9,
+				width = 0.9,
+			},
+		})
+
+		-- Basic keybindings
+		vim.keymap.set('n', '<A-i>', '<CMD>lua require("FTerm").toggle()<CR>')
+		vim.keymap.set('t', '<A-i>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
+		-- floating htop
+		local htop = fterm:new({
+			ftype = 'fterm_htop',
+			cmd = 'htop',
+		})
+		vim.keymap.set('n', '<A-t>', function()
+			htop:toggle()
+		end)
+	end,
+}
