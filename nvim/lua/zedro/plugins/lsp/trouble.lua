eff9e7c07cc0366ca2b5b19ecf4983b54ed7efc2
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-12-08 10:57:31 +0000

    trouble.lua : todo-comment keymapping

--- nvim/lua/zedro/plugins/lsp/trouble.lua
+++ nvim/lua/zedro/plugins/lsp/trouble.lua
@@ -79,4 +79,6 @@ return {
 	vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end),
 	vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end),
 	vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end),
+	-- todo-comments
+	vim.keymap.set("n", "<leader>xt", ":TodoTrouble<cr>", { desc = "Todo Trouble" }),
 }
