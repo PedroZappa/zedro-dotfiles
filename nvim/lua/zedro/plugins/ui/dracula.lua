527d21187147e9c45a49782f8ebdd335b9e3786f
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-12-08 19:51:44 +0000

    starship.toml : riced

--- nvim/lua/zedro/plugins/ui/dracula.lua
+++ nvim/lua/zedro/plugins/ui/dracula.lua
@@ -83,7 +83,7 @@ return {
 			highlight NvimTreeNormal guibg=NONE ctermbg=NONE
 			highlight CursorLineNr guifg=#e8eb34
 			highlight NvimTreeLineNr guifg=#34eb43
-			highlight LineNr guifg=#BD93F9
+			highlight LineNr guifg=#D6ACFF
 		]])
 
 	end,
