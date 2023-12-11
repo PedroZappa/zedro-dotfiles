09fc9c9a751c10c6a0ae8d64da0296c2efd33592
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-12-11 20:26:36 +0000

    .tmux.conf.local : 󰟪

--- nvim/lua/zedro/plugins/init.lua
+++ nvim/lua/zedro/plugins/init.lua
@@ -1,5 +1,6 @@
 return {
 	"christoomey/vim-tmux-navigator", -- tmux & split window navigation
+	'tmux-plugins/vim-tmux',
 	"nvim-lua/plenary.nvim", -- lua functions that many plugins use
 	'vim-scripts/ReplaceWithRegister', -- replace with register contents using motion (gr + motion)
 	'ryanoasis/vim-devicons', -- icons for vim plugins
