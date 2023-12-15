2cfd1166294592882c8cc951ab630abe795a20be
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-12-15 14:05:06 +0000

    lualine.lua : Config Defaults exposed 

--- nvim/lua/zedro/plugins/ui/lualine.lua
+++ nvim/lua/zedro/plugins/ui/lualine.lua
@@ -6,20 +6,28 @@ return {
 		local lazy_status = require("lazy.status") -- to configure lazy pending updates count
 
 		lualine.setup({
+			icons_enabled = true,
 			options = {
 				theme = 'dracula',
+				component_separators = { left = '', right = '' },
+				section_separators = { left = '', right = '' },
+				disabled_filetypes = {
+					statusline = {},
+					winbar = {},
+				},
+				ignore_focus = {},
+				always_divide_middle = true,
+				globalstatus = false,
+				refresh = {
+					statusline = 1000,
+					tabline = 1000,
+					winbar = 1000,
+				}
 			},
 			sections = {
-				lualine_x = {
-					{
-						lazy_status.updates,
-						cond = lazy_status.has_updates,
-						color = { fg = "#ff9e64" },
-					},
-					{ "encoding" },
-					{ "fileformat" },
-					{ "filetype" },
-				},
+				-- Left side
+				lualine_a = { 'mode' },
+				lualine_b = { 'branch', 'diff', 'diagnostics' },
 				lualine_c = {
 					{
 						function()
@@ -41,7 +49,33 @@ return {
 						color = { fg = "#ff9e64" },
 					},
 				},
+				-- Right side
+				lualine_x = {
+					{
+						lazy_status.updates,
+						cond = lazy_status.has_updates,
+						color = { fg = "#ff9e64" },
+					},
+					{ "encoding" },
+					{ "fileformat" },
+					{ "filetype" },
+				},
+				lualine_y = { 'progress' },
+				lualine_z = { 'location' },
 			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { 'filename' },
+				lualine_x = { 'location' },
+				lualine_y = {},
+				lualine_z = {}
+			},
+			tabline = {},
+			winbar = {},
+			inactive_winbar = {},
+			extensions = {}
+
 		})
 	end,
 }
