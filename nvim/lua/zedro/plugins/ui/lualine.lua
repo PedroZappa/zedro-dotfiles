3590a0790dc2c138430a1b489c7cc05697977212
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-12-15 22:35:50 +0000

    lualine.lua : 󰟪d

--- nvim/lua/zedro/plugins/ui/lualine.lua
+++ nvim/lua/zedro/plugins/ui/lualine.lua
@@ -86,7 +86,7 @@ return {
 						'filename',
 						file_status = true, -- Displays file status (readonly status, modified status)
 						newfile_status = true, -- Display new file status (new file means no write after created)
-						path = 4,        -- 0: Just the filename
+						path = 3,        -- 0: Just the filename
 						-- 1: Relative path
 						-- 2: Absolute path
 						-- 3: Absolute path, with tilde as the home directory
