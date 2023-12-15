9b29b6ad93def94f9c4956cf564ce42f6c5d6ab4
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-12-15 14:56:50 +0000

    lualine.lua : 󰟪d

--- nvim/lua/zedro/plugins/ui/lualine.lua
+++ nvim/lua/zedro/plugins/ui/lualine.lua
@@ -10,7 +10,9 @@ return {
 			options = {
 				theme = 'dracula',
 				component_separators = { left = '', right = '' },
+				-- component_separators = { left = '⵰', right = '⵰' },
 				section_separators = { left = '', right = '' },
+				-- section_separators = { left = '', right = '' },
 				disabled_filetypes = {
 					statusline = {},
 					winbar = {},
@@ -72,7 +74,29 @@ return {
 				lualine_z = {}
 			},
 			tabline = {},
-			winbar = {},
+			winbar = {
+				lualine_z = {
+					{
+						'filename',
+						file_status = true, -- Displays file status (readonly status, modified status)
+						newfile_status = true, -- Display new file status (new file means no write after created)
+						path = 4,         -- 0: Just the filename
+						-- 1: Relative path
+						-- 2: Absolute path
+						-- 3: Absolute path, with tilde as the home directory
+						-- 4: Filename and parent dir, with tilde as the home directory
+
+						shorting_target = 70, -- Shortens path to leave n spaces in the window
+						-- for other components. (terrible name, any suggestions?)
+						symbols = {
+							modified = '[⊕]', -- Text to show when the file is modified.
+							readonly = '[⊘]', -- Text to show when the file is non-modifiable or readonly.
+							unnamed = '[]', -- Text to show for unnamed buffers.
+							newfile = '[New]', -- Text to show for newly created file before first write
+						}
+					}
+				},
+			},
 			inactive_winbar = {},
 			extensions = {}
 
