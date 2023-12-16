d7728c339f0323af98859b035ef987f7f838cc11
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-12-16 12:14:18 +0000

    bufferline.lua : lsp feedback enabled 

--- nvim/lua/zedro/plugins/ui/bufferline.lua
+++ nvim/lua/zedro/plugins/ui/bufferline.lua
@@ -2,15 +2,18 @@ return {
 	"akinsho/bufferline.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	version = "*",
+	event = "VeryLazy",
 	opts = { -- require(bufferline).setup(opts)
 		options = {
 			mode = "buffers",
+			themable = true,
 			buffer_close_icon = '󰅖',
 			modified_icon = '●',
 			close_icon = '',
 			left_trunc_marker = '',
 			right_trunc_marker = '',
 			tab_size = 18,
+			diagnostics = "nvim_lsp",
 			separator_style = "slant",
 			hover = {
 				enabled = true,
