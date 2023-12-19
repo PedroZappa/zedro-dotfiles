1c8bfa00c835b6b400dd32df90320e72e9489411
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-12-19 12:41:55 +0000

    nvim/*/treesitter.lua : Incremental selection added

--- nvim/lua/zedro/plugins/treesitter.lua
+++ nvim/lua/zedro/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
 	'nvim-treesitter/nvim-treesitter',
 	build = ":TSUpdate",
-	config = function ()
+	config = function()
 		local configs = require("nvim-treesitter.configs")
 
 		configs.setup({
@@ -14,6 +14,15 @@ return {
 			auto_install = true,
 			highlight = { enable = true },
 			indent = { enable = true },
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "gnn",
+					node_incremental = "grn",
+					scope_incremental = "grc",
+					node_decremental = "grm",
+				},
+			},
 		})
 	end,
 }
