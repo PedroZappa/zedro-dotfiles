38bc93fc4be047fadd9f672b567677591b141f6b
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-12-19 13:50:06 +0000

    nvim/*/treesitter.lua : 󰟪'ed textobjects module

--- nvim/lua/zedro/plugins/treesitter.lua
+++ nvim/lua/zedro/plugins/treesitter.lua
@@ -1,5 +1,8 @@
 return {
 	'nvim-treesitter/nvim-treesitter',
+	dependencies = {
+		'nvim-treesitter/nvim-treesitter-textobjects',
+	},
 	build = ":TSUpdate",
 	config = function()
 		local configs = require("nvim-treesitter.configs")
@@ -21,6 +24,36 @@ return {
 					node_incremental = "<leader>si",
 					scope_incremental = "<leader>sc",
 					node_decremental = "<leader>sd",
+				},
+			},
+			-- https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+			textobjects = {
+				select = {
+					enable = true,
+					-- Automatically jump forward to textobj, similar to targets.vim
+					lookahead = true,
+					keymaps = {
+						-- You can use the capture groups defined in textobjects.scm
+						["af"] = { query = "@function.outer", desc = "Select outer part of a function region" },
+						["if"] = { query = "@function.inner", desc = "Select inner part of a function region" },
+						["ac"] = { query = "@class.outer", desc = "Select outer part of a class region" },
+						["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+						-- You can also use captures from other query groups like `locals.scm`
+						["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+					},
+					-- You can choose the select mode (default is charwise 'v')
+					--
+					-- Can also be a function which gets passed a table with the keys
+					-- * query_string: eg '@function.inner'
+					-- * method: eg 'v' or 'o'
+					-- and should return the mode ('v', 'V', or '<c-v>') or a table
+					-- mapping query_strings to modes.
+					selection_modes = {
+						['@parameter.outer'] = 'v', -- charwise
+						['@function.outer'] = 'v', -- linewise
+						['@class.outer'] = '<c-v>', -- blockwise
+					},
+					include_surrounding_whitespace = true,
 				},
 			},
 		})
