c2b874ad4fbd2ea8fb5b67b10145c0531127cbf6
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-12-29 23:36:01 +0000

    markdown-toc.lua : added

--- nvim/lua/zedro/plugins/ui/indent-blankline.lua
+++ nvim/lua/zedro/plugins/ui/indent-blankline.lua
@@ -1,23 +1,25 @@
 return {
-	"lukas-reineke/indent-blankline.nvim",
-	main = "ibl",
-	lazy = true,
-	event = { "BufReadPre", "BufNewFile" },
-	opts = {
-	},
-	config = function()
-		local highlight = {
-			-- "LightPurple",
-			"DarkPurple",
-		}
-		local hooks = require "ibl.hooks"
-		-- create the highlight groups in the highlight setup hook, so they are reset
-		-- every time the colorscheme changes
-		hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-			-- vim.api.nvim_set_hl(0, "LightPurple", { fg = "#715da8" })
-			vim.api.nvim_set_hl(0, "DarkPurple", { fg = "#5232a8" })
-		end)
-
-		require("ibl").setup { indent = { highlight = highlight } }
-	end,
+	-- "lukas-reineke/indent-blankline.nvim",
+	-- main = "ibl",
+	-- lazy = true,
+	-- event = { "BufReadPre", "BufNewFile" },
+	-- ft = { "*" },
+	-- exclude_ft = { "markdown" },
+	-- opts = {
+	-- },
+	-- config = function()
+	-- 	local highlight = {
+	-- 		-- "LightPurple",
+	-- 		"DarkPurple",
+	-- 	}
+	-- 	local hooks = require "ibl.hooks"
+	-- 	-- create the highlight groups in the highlight setup hook, so they are reset
+	-- 	-- every time the colorscheme changes
+	-- 	hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+	-- 		-- vim.api.nvim_set_hl(0, "LightPurple", { fg = "#715da8" })
+	-- 		vim.api.nvim_set_hl(0, "DarkPurple", { fg = "#5232a8" })
+	-- 	end)
+	--
+	-- 	require("ibl").setup { indent = { highlight = highlight } }
+	-- end,
 }
