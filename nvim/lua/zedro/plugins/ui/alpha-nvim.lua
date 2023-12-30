389968e934b23716aa47924ae2b655b3f809c5e1
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-12-30 20:48:49 +0000

    nvim/*/alpha-nvim.lua : Added

--- nvim/lua/zedro/plugins/ui/alpha-nvim.lua
+++ nvim/lua/zedro/plugins/ui/alpha-nvim.lua
@@ -2,6 +2,33 @@ return {
   "goolord/alpha-nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    require("alpha").setup(require("alpha.themes.dashboard").config)
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.dashboard")
+
+    -- Set header
+    -- dashboard.section.header.val = {
+    --   "                                                     ",
+    --   "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+    --   "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+    --   "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+    --   "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+    --   "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+    --   "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+    --   "                                                     ",
+    -- }
+
+    -- Set menu
+    dashboard.section.buttons.val = {
+      { type = "text", val = "Quick links", opts = { hl = "SpecialComment", position = "center" } },
+      { type = "padding", val = 1 },
+      dashboard.button("f", "🔍  Find file", ":Telescope find_files <CR>"),
+      dashboard.button("F", "🔤  Find text", ":Telescope live_grep <CR>"),
+      dashboard.button("n", "💡  New file", ":ene <BAR> startinsert <CR>"),
+      dashboard.button("t", "📺  Start Screen", ":e ~/.config/nvim/lua/core/startup-screen.lua <CR>"),
+      dashboard.button("l", "♻   Get Lazy", ":Lazy<CR>"),
+      dashboard.button("q", "🚪  Quit", ":qa<CR>"),
+    }
+    -- Send config to alpha
+    alpha.setup(dashboard.opts)
   end,
 }
