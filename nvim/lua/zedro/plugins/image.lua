2050104f1fde2d4275b4117ff8fa445f03130990
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-12-31 11:49:42 +0000

    nvim/*/image.lua : 󰟪'ed

---
+++ nvim/lua/zedro/plugins/image.lua
@@ -0,0 +1,22 @@
+return {
+  "samodostal/image.nvim",
+  requires = { "nvim-lua/plenary.nvim" },
+  dependencies = {
+    { "m00qek/baleia.nvim", tag = "v1.3.0" }, -- color support
+  },
+  config = function()
+    require("image").setup({
+      render = {
+        min_padding = 5,
+        show_label = true,
+        show_image_dimensions = true,
+        use_dither = true,
+        foreground_color = true,
+        background_color = true,
+      },
+      events = {
+        update_on_nvim_resize = true,
+      },
+    })
+  end,
+}
