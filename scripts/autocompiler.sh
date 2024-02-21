9d983413923718b0e80f7447b3323309f08b0156
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-02-21 13:24:12 +0000

    scripts/autocompiler.sh : Added

---
+++ scripts/autocompiler.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Auto Compiler powered by entr
+# https://eradman.com/entrproject/
+
+# If process already running, kill it and exit
+pkill -f "entr compiler" && exit
+# Start compiling process
+echo "$1" | entr compiler "$1"
