d548970e834961e8694f8cec347fc2adf2e170c3
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-03-05 14:27:20 +0000

    Nvim mason : cleanup

--- nvim/lua/zedro/plugins/lsp/mason.lua
+++ nvim/lua/zedro/plugins/lsp/mason.lua
@@ -25,11 +25,9 @@ return {
       -- list of servers for mason to install
       ensure_installed = {
         "clangd",
+        "lua_ls",
         "html",
         "cssls",
-        "lua_ls",
-        "graphql",
-        "pylyzer",
       },
       -- auto-install configured servers (with lspconfig)
       automatic_installation = true, -- not the same as ensure_installed
@@ -39,9 +37,6 @@ return {
       ensure_installed = {
         "prettier", -- prettier formatter
         "stylua", -- lua formatter
-        "isort", -- python formatter
-        "black", -- python formatter
-        "pylint", -- python linter
         "eslint_d", -- js linter
       },
     })
