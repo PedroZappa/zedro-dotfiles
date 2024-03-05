250a185a4d15420db0317671d3dff991072764d5
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-03-05 14:33:47 +0000

    nvim-cmp : cleanup

--- nvim/lua/zedro/plugins/nvim-cmp.lua
+++ nvim/lua/zedro/plugins/nvim-cmp.lua
@@ -31,30 +31,28 @@ return {
 					luasnip.lsp_expand(args.body)
 				end,
 			},
-			mapping = cmp.mapping.preset.insert({
-				["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-				["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-				["<C-b>"] = cmp.mapping.scroll_docs(-4),
-				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-				["<C-e>"] = cmp.mapping.abort(),    -- close completion window
-				["<CR>"] = cmp.mapping.confirm({ select = false }),
-			}),
-			-- sources for autocompletion
-			sources = cmp.config.sources({
+			sources = cmp.config.sources({ -- sources for autocompletion
 				{ name = "nvim_lsp" },
 				{ name = "luasnip" }, -- snippets
 				{ name = "buffer" }, -- text within current buffer
 				{ name = "path" }, -- file system paths
 				-- { name = "codeium" }, -- codeium
 			}),
-			-- configure lspkind for vs-code like pictograms in completion menu
-			formatting = {
+			formatting = {	-- configure lspkind for vs-code like pictograms in completion menu
 				format = lspkind.cmp_format({
 					maxwidth = 50,
 					ellipsis_char = "...",
 				}),
 			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-n>"] = cmp.mapping.select_next_item(),
+				["<C-p>"] = cmp.mapping.select_prev_item(),
+				["<C-b>"] = cmp.mapping.scroll_docs(-2),
+				["<C-f>"] = cmp.mapping.scroll_docs(2),
+				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+				["<C-e>"] = cmp.mapping.abort(),		-- close completion window
+				["<C-y>"] = cmp.mapping.confirm({ select = false }),
+			}),
 		})
 	end,
 }
