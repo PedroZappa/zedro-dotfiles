8f3273fd218598a43902a1b7f4eeee3a5b45f828
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-03-15 10:51:50 +0000

    nvim-surround.lua : cleanedup

--- nvim/lua/zedro/plugins/edit/nvim-surround.lua
+++ nvim/lua/zedro/plugins/edit/nvim-surround.lua
@@ -1,6 +1,13 @@
+-- Config Ref
+-- https://github.com/kylechui/nvim-surround/blob/main/lua/nvim-surround/config.lua
 return {
 	"kylechui/nvim-surround",
-	event = { "BufReadPre", "BufNewFile" },
 	version = "*", -- Use for stability; omit to use `main` branch for the latest features
-	config = true,
+	event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
 }
+
