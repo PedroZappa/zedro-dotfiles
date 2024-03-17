e8063b860c3894b3c20b5327897e67d1cb325ad3
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-03-17 10:16:40 +0000

    nvim-cmp.lua : fix

--- nvim/lua/zedro/plugins/nvim-cmp.lua
+++ nvim/lua/zedro/plugins/nvim-cmp.lua
@@ -11,7 +11,7 @@ return {
 		"saadparwaiz1/cmp_luasnip",			-- for autocompletion
 		"rafamadriz/friendly-snippets",		-- useful snippets
 		"onsails/lspkind.nvim",				-- vs-code like pictograms
-		-- 'quangnguyen30192/cmp-nvim-tags',	-- nvim-tags
+		{ "quangnguyen30192/cmp-nvim-tags", ft = "markdown" },	-- nvim-tags
 	},
 	config = function()
 		-- local cmp_autopairs = require('nvim-autopairs.completion.cmp')
@@ -46,6 +46,21 @@ return {
 				{ name = "buffer" }, -- text within current buffer
 				{ name = "path" }, -- file system paths
 				{ name = "cmdline" }, -- cmdline
+				{
+					name = 'tags',
+					option = {
+					  -- this is the default options, change them if you want.
+					  -- Delayed time after user input, in milliseconds.
+					  complete_defer = 100,
+					  -- Max items when searching `taglist`.
+					  max_items = 10,
+					  -- Use exact word match when searching `taglist`, for better searching
+					  -- performance.
+					  exact_match = false,
+					  -- Prioritize searching result for current buffer.
+					  current_buffer_only = false,
+				},
+			  },
 				-- { name = "codeium" }, -- codeium
 			}),
 			completion = {
