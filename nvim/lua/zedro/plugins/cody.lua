0e4b44a84f488f7352262b88818ea780660c6772
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-04-29 08:31:44 +0100

    nvim/*/lazy-lock.json : updated

--- nvim/lua/zedro/plugins/cody.lua
+++ nvim/lua/zedro/plugins/cody.lua
@@ -1,8 +1,8 @@
-return {
-    "sourcegraph/sg.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-	build = "nvim -l build/init.lua",
-	config = function()
-		require("sg").setup()
-	end
-}
+-- return {
+--     "sourcegraph/sg.nvim",
+--     dependencies = { "nvim-lua/plenary.nvim" },
+-- 	build = "nvim -l build/init.lua",
+-- 	config = function()
+-- 		require("sg").setup()
+-- 	end
+-- }
