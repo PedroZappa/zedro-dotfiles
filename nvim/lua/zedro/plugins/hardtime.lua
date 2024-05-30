88a4b3b6d746b11c81ba484bbc8d4d3d9c99ce7f
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-05-30 21:51:23 +0100

    nvim/ *** /hartime.lua : Edited

--- nvim/lua/zedro/plugins/hardtime.lua
+++ nvim/lua/zedro/plugins/hardtime.lua
@@ -1,10 +1,12 @@
 return {
 	"m4xshen/hardtime.nvim",
-	command = "Hardtime",
-	event = "BufEnter",
+	-- command = "Hardtime",
+	-- event = "BufEnter",
 	dependencies = {
 		"MunifTanjim/nui.nvim",
 		"nvim-lua/plenary.nvim"
 	},
-	opts = {}
+	opts = {
+		disable_mouse = true,
+	}
 }
