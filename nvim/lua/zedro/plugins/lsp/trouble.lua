0a9f06b94414fa98ee53b4b8274fe4439d13caa9
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-05-31 21:24:03 +0100

    nvim/ *** /lsp/trouble.lua : deprecation update

--- nvim/lua/zedro/plugins/lsp/trouble.lua
+++ nvim/lua/zedro/plugins/lsp/trouble.lua
@@ -60,7 +60,7 @@ return {
 	config = function()
 		-- Telescope integration
 		local actions = require("telescope.actions")
-		local trouble = require("trouble.providers.telescope")
+		local trouble = require("trouble.sources.telescope")
 
 		local telescope = require("telescope")
 
