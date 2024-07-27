7bf789ff82eb2fc1af2d7e83b83d2548ca43819d
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-07-27 23:19:14 +0100

    scripts/devenv.sh : kitty

--- scripts/devenv.sh
+++ scripts/devenv.sh
@@ -268,9 +268,20 @@ fi
 install_kitty() {
 	echo "Installing kitty Terminal emulator..."
 	curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
-	sudo ln -s ~/.local/kitty.app/bin/kitty /usr/local/bin/
+	ln -s ~/.local/kitty.app/bin/kitty /usr/local/bin/
 	ln -s ~/.dotfiles/kitty/dracula.conf ~/.config/kitty/dracula.conf
 	ln -s ~/.dotfiles/kitty/diff.conf ~/.config/kitty/diff.conf
+	## Desktop Integration
+	ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten ~/.local/bin/
+	# Place the kitty.desktop file somewhere it can be found by the OS
+	cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
+	# If you want to open text files and images in kitty via your file manager also add the kitty-open.desktop file
+	cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/applications/
+	# Update the paths to the kitty and its icon in the kitty desktop file(s)
+	sed -i "s|Icon=kitty|Icon=$(readlink -f ~)/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
+	sed -i "s|Exec=kitty|Exec=$(readlink -f ~)/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
+	# Make xdg-terminal-exec (and hence desktop environments that support it use kitty)
+	echo 'kitty.desktop' > ~/.config/xdg-terminals.list
 	echo "kitty installation complete 😻"
 }
 
