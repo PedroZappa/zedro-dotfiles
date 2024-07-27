823f3baabb90df0a2436be57123f165aad5362e5
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-07-27 22:21:37 +0100

    scripts/devenv.sh : cleanup

--- scripts/devenv.sh
+++ scripts/devenv.sh
@@ -185,7 +185,6 @@ install_oh_my_tmux() {
     cd "$HOME"
     git clone https://github.com/gpakosz/.tmux.git
     ln -s -f .tmux/.tmux.conf
-    cp .tmux/.tmux.conf.local .
     echo "oh-my-tmux installation complete."
 }
 
