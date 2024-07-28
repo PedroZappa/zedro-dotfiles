064d88e226bf04b6031678779a1278939f24f01d
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-07-28 15:44:44 +0100

    scripts/devenv.sh : color test

--- scripts/devenv.sh
+++ scripts/devenv.sh
@@ -4,6 +4,31 @@ set -euo pipefail
 # -u : Treat unset variables as an error and exit;
 # -o pipeline : Set the exit status to the last command in the pipeline that failed.
 
+# Color Codes
+# Run the following command to get list of available colors
+# bash -c 'for c in {0..255}; do tput setaf $c; tput setaf $c | cat -v; echo =$c; done'
+
+B=$(tput bold)
+BLA=$(tput setaf 0)
+RED=$(tput setaf 1)
+GRN=$(tput setaf 2)
+YEL=$(tput setaf 3)
+BLU=$(tput setaf 4)
+MAG=$(tput setaf 5)
+CYA=$(tput setaf 6)
+WHI=$(tput setaf 7)
+GRE=$(tput setaf 8)
+BRED=$(tput setaf 9)
+BGRN=$(tput setaf 10)
+BYEL=$(tput setaf 11)
+BBLU=$(tput setaf 12)
+BMAG=$(tput setaf 13)
+BCYA=$(tput setaf 14)
+BWHI=$(tput setaf 15)
+D=$(tput sgr0)
+BEL=$(tput bel)
+CLR=$(tput el 1)
+
 ### Dev Environment Setup
 # Sources:
 # Brew without sudo
@@ -42,7 +67,7 @@ if [[ $# -gt 0 && "$1" == "--express" ]]; then
     EXPRESS_INSTALL=true
 fi
 
-echo "Installing dev environment... 🖳 "
+echo "${B}Installing dev environment...${D}${GRN}🖳 ${D}"
 echo "Do you want to express install? (y/n)"
 read -r response
 if [[ "$response" =~ ^[Yy]$ ]]; then
@@ -91,7 +116,6 @@ clone_dotfiles() {
 	fi
 }
 
-echo "Starting Dev Environment Setup"
 if [[ "$EXPRESS_INSTALL" == false ]]; then
     echo "Do you want to clone Zedro's .dotfiles repository? (y/n)"
     read -r response
