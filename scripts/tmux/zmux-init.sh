7e0d87a687417dc6f9b9d5e564a1cd8f858eff6a
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-08-04 10:40:10 +0100

    scripts/tmux/zmux-init.sh : Improved

--- scripts/tmux/zmux-init.sh
+++ scripts/tmux/zmux-init.sh
@@ -1,5 +1,36 @@
 #!/usr/bin/env bash
 # set -euo pipefail
+# -e : Exit immediately if a command exits with a non-zero status;
+# -u : Treat unset variables as an error and exit;
+# -o pipeline : Set the exit status to the last command in the pipeline that failed.
+
+# Color Codes
+# Run the following command to get list of available colors
+# bash -c 'for c in {0..255}; do tput setaf $c; tput setaf $c | cat -v; echo =$c; done'
+
+B=$(tput bold)
+BLA=$(tput setaf 0)
+RED=$(tput setaf 1)
+GRN=$(tput setaf 2)
+YEL=$(tput setaf 3)
+BLU=$(tput setaf 4)
+MAG=$(tput setaf 5)
+CYA=$(tput setaf 6)
+WHI=$(tput setaf 7)
+GRE=$(tput setaf 8)
+PRP=$(tput setaf 99)
+BRED=$(tput setaf 9)
+BGRN=$(tput setaf 10)
+BYEL=$(tput setaf 11)
+BBLU=$(tput setaf 12)
+BMAG=$(tput setaf 13)
+BCYA=$(tput setaf 14)
+BWHI=$(tput setaf 15)
+D=$(tput sgr0)
+BEL=$(tput bel)
+
+# Initialization message
+echo ${YEL}ZMUX${D}${PRP}: Initializing Dev Env...${D} ${GRN}${D}
 
 # Set Path to Obsidian Vault
 if [[ $USER == "zedr0" ]]; then			# DEV-Desk
@@ -14,12 +45,8 @@ fi
 
 export OBSIDIAN_VAULT_PATH
 
-I3SOCK=$(ls /run/user/1000/i3/ipc-socket.*)
-export I3SOCK
-
-# Session Name variables
-SESH1="RC"
-SESH2="DEV"
+# I3SOCK=$(ls /run/user/1000/i3/ipc-socket.*)
+# export I3SOCK
 
 # Command line argument for working directory
 if [[ $# -gt 0 ]]; then
@@ -28,14 +55,25 @@ else
     DEV_DIR=$HOME  # Default directory if none is provided
 fi
 
+# Extract project name from the path
+if command -v zoxide &> /dev/null; then
+    FULL_DEV_DIR=$(zoxide query "$DEV_DIR") # Use zoxide to get the full path
+else
+    FULL_DEV_DIR="$DEV_DIR" # Fallback to the provided DEV_DIR if zoxide is not installed
+fi
+PROJECT_NAME=$(basename "$FULL_DEV_DIR") # Extract the project name
+
+# Session Name variables
+SESH1="RC"
+SESH2="DEV"
+
 # Create RC session
 tmux new-session	-d -s $SESH1
 # Create .dotfiles RC window
 tmux rename-window	-t RC:1 '.dotfiles'
 tmux send-keys		-t RC:1 'cd $HOME/.dotfiles' C-m
 tmux send-keys		-t RC:1 'git pull' C-m
-tmux send-keys		-t RC:2 'glgs' C-m
-tmux send-keys		-t RC:1 'nvim' C-m
+tmux send-keys		-t RC:1 $EDITOR C-m
 # Create obsidian RC window
 tmux new-window		-t RC:2 -n 'obsidian' -c $OBSIDIAN_VAULT_PATH
 tmux send-keys		-t RC:2 'cd '$OBSIDIAN_VAULT_PATH C-m
@@ -59,15 +97,18 @@ fi
 # Create DEV session
 tmux new-session	-d -s $SESH2
 # Create Working Project window
-tmux rename-window	-t DEV:1 '...'
+tmux rename-window	-t DEV:1 "$PROJECT_NAME"
 tmux send-keys		-t DEV:1 'cd '$DEV_DIR C-m
+tmux send-keys		-t DEV:1 '' C-m
 # Create Debug window
 tmux new-window		-t DEV:2 -n 'DEBUG'
 tmux split-window	-t DEV:2 -h
 tmux send-keys		-t DEV:2 'cd '$DEV_DIR C-m
 tmux send-keys		-t DEV:2.1 'cd '$DEV_DIR C-m
-tmux resize-pane	-L 120
-
+tmux send-keys		-t DEV:2.1 $EDITOR C-m
+tmux send-keys		-t DEV:2.1 ":e .vgdbinit" C-m
+tmux send-keys		-t DEV:2.1 ":split .gdbinit" C-m
+tmux resize-pane	-L 160
 # Create SYNC window
 tmux new-window		-t DEV:3 -n 'SYNC'
 tmux send-keys		-t DEV:3 'cd '$DEV_DIR C-m
@@ -75,8 +116,4 @@ tmux send-keys		-t DEV:3 'cd '$DEV_DIR C-m
 # Attach to DEV session
 tmux attach-session -t DEV:1
 
-# Open termina on i3 workspace2 and attach to DEV session
-# i3-msg --socket=$I3SOCK exec i3-sensible-terminal
-
-# i4-msg workspace 2; i3-msg exec i3-sensible-terminal
-# tmux attach-session -t DEV
+echo ${YEL}ZMUX${D}${PRP}: Dev Env Destroyed!${D} 💣
