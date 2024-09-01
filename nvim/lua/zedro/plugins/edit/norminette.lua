a12e117d908f8f3259b97e8ab1356cfab6993463
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-09-01 11:08:30 +0100

    nvim/*/norminette.lua : added

---
+++ nvim/lua/zedro/plugins/edit/norminette.lua
@@ -0,0 +1,7 @@
+return {
+	-- "MrSloth-dev/NorminetteDiagnostics",
+	-- config = function()
+	-- 	require("norminette").setup()
+	-- end,
+	-- vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+}
