28b1775d90b2715ab7a4361c33ef2a74952c0051
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-09-01 10:52:01 +0100

    nvim/lazy-lock.json : updated removed conflicting hardtime.lua plugin

--- nvim/lua/zedro/plugins/hardtime.lua
+++ nvim/lua/zedro/plugins/hardtime.lua
@@ -1,13 +1,13 @@
 return {
-	"m4xshen/hardtime.nvim",
-	-- command = "Hardtime",
-	-- event = "BufEnter",
-	dependencies = {
-		"MunifTanjim/nui.nvim",
-		"nvim-lua/plenary.nvim"
-	},
-	opts = {
-		disable_mouse = false,
-		disabled_keys = {},
-	}
+	-- "m4xshen/hardtime.nvim",
+	-- -- command = "Hardtime",
+	-- -- event = "BufEnter",
+	-- dependencies = {
+	-- 	"MunifTanjim/nui.nvim",
+	-- 	"nvim-lua/plenary.nvim"
+	-- },
+	-- opts = {
+	-- 	disable_mouse = false,
+	-- 	disabled_keys = {},
+	-- }
 }
