dff5efc0985c1c747f847118a5fafd601410ca23
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-09-15 16:48:59 +0100

    ./editorconfig : Added

--- nvim/lua/zedro/plugins/lsp/conform.lua
+++ nvim/lua/zedro/plugins/lsp/conform.lua
@@ -1,38 +1,41 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local conform = require("conform")
-
-    conform.setup({
-      formatters_by_ft = {
-        -- c = {
-        --   indent_style = 'tab',
-        --   tab_width = 4,
-        -- },
-        cpp = { "clang-format" },
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        javascriptreact = { "prettier" },
-        typescriptreact = { "prettier" },
-        svelte = { "prettier" },
-        css = { "prettier" },
-        html = { "prettier" },
-        json = { "prettier" },
-        yaml = { "prettier" },
-        markdown = { "prettier" },
-        graphql = { "prettier" },
-        lua = { "stylua" },
-        python = { "isort", "black" },
-        bash = { "beautysh" },
-      },
-    })
-    vim.keymap.set({ "n", "v" }, "<leader>pf", function()
-      conform.format({
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 500,
-      })
-    end, { desc = "Make file or range PRETTY (v/n mode)" })
-  end,
+  -- "stevearc/conform.nvim",
+  -- event = { "BufReadPre", "BufNewFile" },
+  -- config = function()
+  --   local conform = require("conform")
+  --
+  --   conform.setup({
+  --     formatters_by_ft = {
+  --       -- c = {
+  --       --   indent_style = 'tab',
+  --       --   tab_width = 4,
+  --       -- },
+  --       cpp = { "clang-format" },
+  --       javascript = { "prettier" },
+  --       typescript = { "prettier" },
+  --       javascriptreact = { "prettier" },
+  --       typescriptreact = { "prettier" },
+  --       svelte = { "prettier" },
+  --       css = { "prettier" },
+  --       html = { "prettier" },
+  --       json = { "prettier" },
+  --       yaml = { "prettier" },
+  --       markdown = { "prettier" },
+  --       graphql = { "prettier" },
+  --       lua = { "stylua",
+		-- 	indent_style = "space",
+		-- 	tab_width = 2
+		-- },
+  --       python = { "isort", "black" },
+  --       bash = { "beautysh" },
+  --     },
+  --   })
+  --   vim.keymap.set({ "n", "v" }, "<leader>pf", function()
+  --     conform.format({
+  --       lsp_fallback = true,
+  --       async = false,
+  --       timeout_ms = 500,
+  --     })
+  --   end, { desc = "Make file or range PRETTY (v/n mode)" })
+  -- end,
 }
--- nvim/lua/zedro/plugins/lsp/lsp-zero.lua
+++ nvim/lua/zedro/plugins/lsp/lsp-zero.lua
@@ -1,15 +1,40 @@
 return {
-  "VonHeikemen/lsp-zero.nvim",
-  branch = "v3.x",
-  lazy = true,
-  config = false,
-  init = function()
-    -- Disable automatic setup, we are doing it manually
-    vim.g.lsp_zero_extend_cmp = 0
-    vim.g.lsp_zero_extend_lspconfig = 0
-  end,
-  keys = {
-    { "<leader>i", "<cmd>LspInfo<CR>", desc = "Lsp-Zero Info" },
-    { "<leader>I", "<cmd>LspInstall<CR>", desc = "Lsp-Zero Install" },
-  },
+	"VonHeikemen/lsp-zero.nvim",
+	branch = "v3.x",
+	lazy = true,
+	init = function()
+		-- Disable automatic setup, we are doing it manually
+		vim.g.lsp_zero_extend_cmp = 0
+		vim.g.lsp_zero_extend_lspconfig = 0
+	end,
+	config = function()
+		local lsp_zero = require('lsp-zero')
+
+		-- lsp_attach is where you enable features that only work
+		-- if there is a language server active in the file
+		local lsp_attach = function(client, bufnr)
+			local opts = { buffer = bufnr }
+
+			vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+			vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+			vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+			vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+			vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+			vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+			vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+			vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+			vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+			vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+		end
+
+		lsp_zero.extend_lspconfig({
+			sign_text = true,
+			lsp_attach = lsp_attach,
+			capabilities = require('cmp_nvim_lsp').default_capabilities(),
+		})
+	end,
+	keys = {
+		{ "<leader>i", "<cmd>LspInfo<CR>",    desc = "Lsp-Zero Info" },
+		{ "<leader>I", "<cmd>LspInstall<CR>", desc = "Lsp-Zero Install" },
+	},
 }
