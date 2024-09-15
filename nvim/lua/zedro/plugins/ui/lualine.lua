fc6458d34c150cf6aab8f06aeeb0e9f59aca937c
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-09-15 11:13:46 +0100

    nvim/*/lualine.lua : improved

--- nvim/lua/zedro/plugins/ui/lualine.lua
+++ nvim/lua/zedro/plugins/ui/lualine.lua
@@ -32,24 +32,10 @@ return {
 				lualine_b = { 'branch', 'diff', 'diagnostics' },
 				lualine_c = {
 					{
-						function()
-							local ok, pomo = pcall(require, "pomo")
-							if not ok then
-								return ""
-							end
-							local timer = pomo.get_first_to_finish()
-							if timer == nil then
-								return ""
-							end
-							return "⏲  " .. tostring(timer)
-						end,
-						"encoding",
+						require("noice").api.statusline.mode.get,
+						cond = require("noice").api.statusline.mode.has,
+						color = { fg = "#ff9e64" },
 					},
-					-- {
-					-- 	require("noice").api.statusline.mode.get,
-					-- 	cond = require("noice").api.statusline.mode.has,
-					-- 	color = { fg = "#ff9e64" },
-					-- },
 				},
 				-- Right side
 				lualine_x = {
@@ -79,29 +65,6 @@ return {
 				lualine_z = {}
 			},
 			tabline = {},
-			-- winbar = {
-			-- 	lualine_z = {
-			-- 		{
-			-- 			'filename',
-			-- 			file_status = true, -- Displays file status (readonly status, modified status)
-			-- 			newfile_status = true, -- Display new file status (new file means no write after created)
-			-- 			path = 3,        -- 0: Just the filename
-			-- 			-- 1: Relative path
-			-- 			-- 2: Absolute path
-			-- 			-- 3: Absolute path, with tilde as the home directory
-			-- 			-- 4: Filename and parent dir, with tilde as the home directory
-			--
-			-- 			shorting_target = 70, -- Shortens path to leave n spaces in the window
-			-- 			-- for other components. (terrible name, any suggestions?)
-			-- 			symbols = {
-			-- 				modified = '[⊕]', -- Text to show when the file is modified.
-			-- 				readonly = '[⊘]', -- Text to show when the file is non-modifiable or readonly.
-			-- 				unnamed = '[]', -- Text to show for unnamed buffers.
-			-- 				newfile = '[New]', -- Text to show for newly created file before first write
-			-- 			}
-			-- 		}
-			-- 	},
-			-- },
 			inactive_winbar = {},
 			extensions = {}
 
