01aeed1a92307e48d5e02f378c379bedc2a309b9
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-09-21 21:04:30 +0100

    nvim/*/codeium.lua : updated to nvim version

--- nvim/lua/zedro/plugins/edit/codeium.lua
+++ nvim/lua/zedro/plugins/edit/codeium.lua
@@ -1,15 +1,19 @@
 return {
-	'Exafunction/codeium.vim',
-	dependencies = {
-        "nvim-lua/plenary.nvim",
-        "hrsh7th/nvim-cmp",
-    },
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local keymap = vim.keymap -- for short
-		keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end, { expr = true })
-		keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
-		keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
-		keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
-	end
+  'Exafunction/codeium.nvim',
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "hrsh7th/nvim-cmp",
+  },
+  event = { "BufReadPre", "BufNewFile" },
+  config = function ()
+    require("codeium").setup({
+      enable_chat = true,
+    })
+    local keymap = vim.keymap -- for short
+    keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end, { expr = true })
+    keymap.set('i', '<c-;>', function () return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
+    keymap.set('i', '<c-,>', function () return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
+    keymap.set('i', '<c-x>', function () return vim.fn['codeium#Clear']() end, { expr = true })
+    keymap.set('n', '<C-c>', ":Codeium Chat<CR>", { desc = "Open Codeium Chat" })
+  end
 }
