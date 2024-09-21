1ba84b5501eb3da8b475f4302b16a6835d7f3ceb
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-09-21 21:13:14 +0100

    nvim/*/codeium.lua : rolled back to .vim

--- nvim/lua/zedro/plugins/edit/codeium.lua
+++ nvim/lua/zedro/plugins/edit/codeium.lua
@@ -1,19 +1,20 @@
 return {
-  'Exafunction/codeium.nvim',
+  -- 'Exafunction/codeium.nvim',
+  'Exafunction/codeium.vim',
   dependencies = {
     "nvim-lua/plenary.nvim",
     "hrsh7th/nvim-cmp",
   },
   event = { "BufReadPre", "BufNewFile" },
   config = function ()
-    require("codeium").setup({
-      enable_chat = true,
-    })
+    -- require("codeium").setup({
+    --   enable_chat = true,
+    -- })
     local keymap = vim.keymap -- for short
     keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end, { expr = true })
     keymap.set('i', '<c-;>', function () return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
     keymap.set('i', '<c-,>', function () return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
     keymap.set('i', '<c-x>', function () return vim.fn['codeium#Clear']() end, { expr = true })
-    keymap.set('n', '<C-c>', ":Codeium Chat<CR>", { desc = "Open Codeium Chat" })
+    -- keymap.set('n', '<C-c>', ":Codeium Chat<CR>", { desc = "Open Codeium Chat" })
   end
 }
