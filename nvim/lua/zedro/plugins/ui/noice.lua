f2db9d259a944840b65c09efc92c676b579195e4
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-09-22 11:25:50 +0100

    nvim/*/noice.lua : cmdline popup

--- nvim/lua/zedro/plugins/ui/noice.lua
+++ nvim/lua/zedro/plugins/ui/noice.lua
@@ -27,7 +27,7 @@ return {
     require("noice").setup({
       cmdline = {
         enabled = true, -- enables the Noice cmdline UI
-        view = "cmdline", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
+        view = "cmdline_popup", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
         opts = {},    -- global options for the cmdline. See section on views
         ---@type table<string, CmdlineFormat>
         format = {
