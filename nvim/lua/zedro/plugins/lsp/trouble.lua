d723fe22ac4787c15c7e96a098cdf557b469150c
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-09-28 10:36:11 +0100

    nvim/*/trouble.lua : exposed config

--- nvim/lua/zedro/plugins/lsp/trouble.lua
+++ nvim/lua/zedro/plugins/lsp/trouble.lua
@@ -32,8 +32,6 @@ return {
         preview = { ms = 100, debounce = true },
       },
       keys = {
-        -- Add your key mappings here
-        -- For example:
         ["?"] = "help",
         r = "refresh",
         R = "toggle_refresh",
@@ -41,7 +39,55 @@ return {
         o = "jump_close",
         ["<esc>"] = "cancel",
         ["<cr>"] = "jump",
-        -- ... (add other key mappings as needed)
+        ["<2-leftmouse>"] = "jump",
+        ["<c-s>"] = "jump_split",
+        ["<c-v>"] = "jump_vsplit",
+        -- go down to next item (accepts count)
+        -- j = "next",
+        ["}"] = "next",
+        ["]]"] = "next",
+        -- go up to prev item (accepts count)
+        -- k = "prev",
+        ["{"] = "prev",
+        ["[["] = "prev",
+        dd = "delete",
+        d = { action = "delete", mode = "v" },
+        i = "inspect",
+        p = "preview",
+        P = "toggle_preview",
+        zo = "fold_open",
+        zO = "fold_open_recursive",
+        zc = "fold_close",
+        zC = "fold_close_recursive",
+        za = "fold_toggle",
+        zA = "fold_toggle_recursive",
+        zm = "fold_more",
+        zM = "fold_close_all",
+        zr = "fold_reduce",
+        zR = "fold_open_all",
+        zx = "fold_update",
+        zX = "fold_update_all",
+        zn = "fold_disable",
+        zN = "fold_enable",
+        zi = "fold_toggle_enable",
+        gb = { -- example of a custom action that toggles the active view filter
+          action = function (view)
+            view:filter({ buf = 0 }, { toggle = true })
+          end,
+          desc = "Toggle Current Buffer Filter",
+        },
+        s = { -- example of a custom action that toggles the severity
+          action = function (view)
+            local f = view:get_filter("severity")
+            local severity = ((f and f.filter.severity or 0) + 1) % 5
+            view:filter({ severity = severity }, {
+              id = "severity",
+              template = "{hl:Title}Filter:{hl} {severity}",
+              del = severity == 0,
+            })
+          end,
+          desc = "Toggle Severity Filter",
+        },
       },
       modes = {
         lsp_references = {
@@ -54,39 +100,80 @@ return {
             include_current = false,
           },
         },
+        -- more advanced example that extends the lsp_document_symbols
         symbols = {
           desc = "document symbols",
           mode = "lsp_document_symbols",
           focus = false,
           win = { position = "right" },
           filter = {
+            -- remove Package since luals uses it for control flow structures
             ["not"] = { ft = "lua", kind = "Package" },
             any = {
+              -- all symbol kinds for help / markdown files
               ft = { "help", "markdown" },
+              -- default set of symbol kinds
               kind = {
-                "Class", "Constructor", "Enum", "Field", "Function", "Interface",
-                "Method", "Module", "Namespace", "Package", "Property", "Struct", "Trait",
+                "Class",
+                "Constructor",
+                "Enum",
+                "Field",
+                "Function",
+                "Interface",
+                "Method",
+                "Module",
+                "Namespace",
+                "Package",
+                "Property",
+                "Struct",
+                "Trait",
               },
             },
           },
         },
-      },
-      icons = {
-        indent = {
-          top = "│ ",
-          middle = "├╴",
-          last = "└╴",
-          fold_open = " ",
-          fold_closed = " ",
-          ws = "  ",
-        },
-        folder_closed = " ",
-        folder_open = " ",
-        kinds = {
-          Array = " ",
-          Boolean = "󰨙 ",
-          Class = " ",
-          -- ... (add other kind icons as needed)
+        -- stylua: ignore
+        icons = {
+          ---@type trouble.Indent.symbols
+          indent        = {
+            top         = "│ ",
+            middle      = "├╴",
+            last        = "└╴",
+            -- last          = "-╴",
+            -- last       = "╰╴", -- rounded
+            fold_open   = " ",
+            fold_closed = " ",
+            ws          = "  ",
+          },
+          folder_closed = " ",
+          folder_open   = " ",
+          kinds         = {
+            Array         = " ",
+            Boolean       = "󰨙 ",
+            Class         = " ",
+            Constant      = "󰏿 ",
+            Constructor   = " ",
+            Enum          = " ",
+            EnumMember    = " ",
+            Event         = " ",
+            Field         = " ",
+            File          = " ",
+            Function      = "󰊕 ",
+            Interface     = " ",
+            Key           = " ",
+            Method        = "󰊕 ",
+            Module        = " ",
+            Namespace     = "󰦮 ",
+            Null          = " ",
+            Number        = "󰎠 ",
+            Object        = " ",
+            Operator      = " ",
+            Package       = " ",
+            Property      = " ",
+            String        = " ",
+            Struct        = "󰆼 ",
+            TypeParameter = " ",
+            Variable      = "󰀫 ",
+          },
         },
       },
     }) -- refer to the configuration section below
