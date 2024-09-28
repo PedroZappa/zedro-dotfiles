56ee78532b41bb1dd548555ed6d26f4f9d266a6e
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-09-28 10:26:35 +0100

    nvim/*/trouble.lua : Turned on

--- nvim/lua/zedro/plugins/lsp/trouble.lua
+++ nvim/lua/zedro/plugins/lsp/trouble.lua
@@ -1,85 +1,128 @@
 return {
-	-- "folke/trouble.nvim",
-	-- event = "VeryLazy",
-	-- dependencies = { "nvim-tree/nvim-web-devicons" },
-	-- opts =	{
-	-- 	position = "bottom", -- position of the list can be: bottom, top, left, right
-	-- 	height = 7, -- height of the trouble list when position is top or bottom
-	-- 	width = 50, -- width of the list when position is left or right
-	-- 	icons = true, -- use devicons for filenames
-	-- 	mode = "workspace_diagnostics", -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
-	-- 	severity = nil, -- nil (ALL) or vim.diagnostic.severity.ERROR | WARN | INFO | HINT
-	-- 	fold_open = "", -- icon used for open folds
-	-- 	fold_closed = "", -- icon used for closed folds
-	-- 	group = true, -- group results by file
-	-- 	padding = true, -- add an extra new line on top of the list
-	-- 	cycle_results = true, -- cycle item list when reaching beginning or end of list
-	-- 	action_keys = { -- key mappings for actions in the trouble list
-	-- 		-- map to {} to remove a mapping, for example:
-	-- 		-- close = {},
-	-- 		close = "q", -- close the list
-	-- 		cancel = "<esc>", -- cancel the preview and get back to your last window / buffer / cursor
-	-- 		refresh = "r", -- manually refresh
-	-- 		jump = { "<cr>", "<tab>", "<2-leftmouse>" }, -- jump to the diagnostic or open / close folds
-	-- 		open_split = { "<c-x>" }, -- open buffer in new split
-	-- 		open_vsplit = { "<c-v>" }, -- open buffer in new vsplit
-	-- 		open_tab = { "<c-t>" }, -- open buffer in new tab
-	-- 		jump_close = {"o"}, -- jump to the diagnostic and close the list
-	-- 		toggle_mode = "m", -- toggle between "workspace" and "document" diagnostics mode
-	-- 		switch_severity = "s", -- switch "diagnostics" severity filter level to HINT / INFO / WARN / ERROR
-	-- 		toggle_preview = "P", -- toggle auto_preview
-	-- 		hover = "K", -- opens a small popup with the full multiline message
-	-- 		preview = "p", -- preview the diagnostic location
-	-- 		open_code_href = "c", -- if present, open a URI with more information about the diagnostic error
-	-- 		close_folds = {"zM", "zm"}, -- close all folds
-	-- 		open_folds = {"zR", "zr"}, -- open all folds
-	-- 		toggle_fold = {"zA", "za"}, -- toggle fold of current file
-	-- 		previous = "k", -- previous item
-	-- 		next = "j", -- next item
-	-- 		help = "?" -- help menu
-	-- 	},
-	-- 	multiline = true, -- render multi-line messages
-	-- 	indent_lines = true, -- add an indent guide below the fold icons
-	-- 	win_config = { border = "single" }, -- window configuration for floating windows. See |nvim_open_win()|.
-	-- 	auto_open = false, -- automatically open the list when you have diagnostics
-	-- 	auto_close = false, -- automatically close the list when you have no diagnostics
-	-- 	auto_preview = true, -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
-	-- 	auto_fold = false, -- automatically fold a file trouble list at creation
-	-- 	auto_jump = {"lsp_definitions"}, -- for the given modes, automatically jump if there is only a single result
-	-- 	include_declaration = { "lsp_references", "lsp_implementations", "lsp_definitions"  }, -- for the given modes, include the declaration of the current symbol in the results
-	-- 	signs = {
-	-- 		-- icons / text used for a diagnostic
-	-- 		error = "",
-	-- 		warning = "",
-	-- 		hint = "",
-	-- 		information = "",
-	-- 		other = "",
-	-- 	},
-	-- 	use_diagnostic_signs = false -- enabling this will use the signs defined in your lsp client
-	-- },
-	-- config = function()
-	-- 	-- Telescope integration
-	-- 	local actions = require("telescope.actions")
-	-- 	local trouble = require("trouble.sources.telescope")
-	--
-	-- 	local telescope = require("telescope")
-	--
-	-- 	telescope.setup {
-	-- 		defaults = {
-	-- 			mappings = {
-	-- 				i = { ["<c-t>"] = trouble.open_with_trouble },
-	-- 				n = { ["<c-t>"] = trouble.open_with_trouble },
-	-- 			},
-	-- 		},
-	-- 	}
-	-- end,
-	-- -- Keymaps
-	-- vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end),
-	-- vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end),
-	-- vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end),
-	-- vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end),
-	-- vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end),
-	-- vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end),
-	-- -- todo-comments
-	-- vim.keymap.set("n", "<leader>xt", ":TodoTrouble<cr>", { desc = "Todo Trouble" }),
+  "folke/trouble.nvim",
+  cmd = "Trouble",
+  event = "VeryLazy",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function ()
+    require("trouble").setup({
+      auto_close = false,
+      auto_open = false,
+      auto_preview = true,
+      auto_refresh = true,
+      auto_jump = false,
+      focus = false,
+      restore = true,
+      follow = true,
+      indent_guides = true,
+      max_items = 200,
+      multiline = true,
+      pinned = false,
+      warn_no_results = true,
+      open_no_results = false,
+      win = {}, -- You can add specific window options here if needed
+      preview = {
+        type = "main",
+        scratch = true,
+      },
+      throttle = {
+        refresh = 20,
+        update = 10,
+        render = 10,
+        follow = 100,
+        preview = { ms = 100, debounce = true },
+      },
+      keys = {
+        -- Add your key mappings here
+        -- For example:
+        ["?"] = "help",
+        r = "refresh",
+        R = "toggle_refresh",
+        q = "close",
+        o = "jump_close",
+        ["<esc>"] = "cancel",
+        ["<cr>"] = "jump",
+        -- ... (add other key mappings as needed)
+      },
+      modes = {
+        lsp_references = {
+          params = {
+            include_declaration = true,
+          },
+        },
+        lsp_base = {
+          params = {
+            include_current = false,
+          },
+        },
+        symbols = {
+          desc = "document symbols",
+          mode = "lsp_document_symbols",
+          focus = false,
+          win = { position = "right" },
+          filter = {
+            ["not"] = { ft = "lua", kind = "Package" },
+            any = {
+              ft = { "help", "markdown" },
+              kind = {
+                "Class", "Constructor", "Enum", "Field", "Function", "Interface",
+                "Method", "Module", "Namespace", "Package", "Property", "Struct", "Trait",
+              },
+            },
+          },
+        },
+      },
+      icons = {
+        indent = {
+          top = "│ ",
+          middle = "├╴",
+          last = "└╴",
+          fold_open = " ",
+          fold_closed = " ",
+          ws = "  ",
+        },
+        folder_closed = " ",
+        folder_open = " ",
+        kinds = {
+          Array = " ",
+          Boolean = "󰨙 ",
+          Class = " ",
+          -- ... (add other kind icons as needed)
+        },
+      },
+    }) -- refer to the configuration section below
+  end,
+  opts = {
+  },
+  keys = {
+    {
+      "<leader>xx",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    {
+      "<leader>xX",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+    {
+      "<leader>cs",
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Symbols (Trouble)",
+    },
+    {
+      "<leader>cl",
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      desc = "LSP Definitions / references / ... (Trouble)",
+    },
+    {
+      "<leader>xL",
+      "<cmd>Trouble loclist toggle<cr>",
+      desc = "Location List (Trouble)",
+    },
+    {
+      "<leader>xQ",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
+    },
+  },
 }
