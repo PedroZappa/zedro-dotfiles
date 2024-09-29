d66554589a7d6147793a580f3f390c1fb0c22432
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-09-29 12:01:02 +0100

    nvim/*/nvim_dap.lua : working w/out args

--- nvim/lua/zedro/plugins/ui/nvim_dap.lua
+++ nvim/lua/zedro/plugins/ui/nvim_dap.lua
@@ -12,7 +12,7 @@ return {
     -- Telescope integration
     'nvim-telescope/telescope-dap.nvim',
     -- Language specific
-    'mfussenegger/nvim-dap-python',
+    -- 'mfussenegger/nvim-dap-python',
   },
   config = function ()
     local dap_ok, dap = pcall(require, "dap")
@@ -31,6 +31,27 @@ return {
       -- see :help mason-nvim-dap on how to create custom handlers
       handlers = {},
     })
+
+    -- Adapter Configurations
+    dap.configuration = {
+      codelldb = {
+        name = 'DAP running w/ codelldb Adapter',
+        type = 'codelldb',
+        request = 'launch',
+        program = "${file}",
+        -- cwd = '${workspaceFolder}',
+        -- stopOnEntry = false
+      },
+      bashdb = {
+        name = 'DAP running w/ bashdb Adapter',
+        type = 'bashdb',
+        request = 'launch',
+        program = "${file}",
+        cwd = '${workspaceFolder}',
+        stopOnEntry = false
+      }
+    }
+
     -- UI : see |:help nvim-dap-ui|
     ui.setup({
       icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
@@ -65,13 +86,6 @@ return {
       end,
     })
 
-    -- Adapter Configurations
-    dap.adapters.lldb = {
-      type = 'executable',
-      command = vim.fn.stdpath('data') .. '/mason/bin/codelldb',
-      name = 'lldb',
-    }
-
     -- Event Listeners
     dap.listeners.before.attach.dapui_config = function ()
       ui.open()
@@ -85,20 +99,51 @@ return {
     dap.listeners.before.event_exited.dapui_config = function ()
       ui.close()
     end
-    dap.listeners.after.event_initialized.dapui_config = ui.open
   end,
   keys = function (_, keys)
     local dap = require('dap')
     local dapui = require('dapui')
+
+    -- Run DAP w/ args function
+    local function start_dap_with_args()
+      local filetype = vim.bo.filetype
+      -- Map filetypes to debug adapter types
+      local adapter_map = {
+        c = 'codelldb',
+        cpp = 'codelldb',
+        bash = 'bashdb',
+        -- Add more mappings as needed
+      }
+      -- Get the debug adapter type for the current filetype
+      local adapter_type = adapter_map[filetype]
+      if not adapter_type then
+        print("No debug adapter for filetype: " .. filetype)
+        return
+      end
+      -- Prompt user for program and arguments
+      local program = vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+      local args = vim.split(vim.fn.input('Arguments: ', vim.fn.getcwd() .. '/'), " ")
+
+      local config = {
+        type = 'codelldb', -- or the appropriate type for your debugger
+        request = 'launch',
+        program = program,
+        args = args,
+        cwd = '${workspaceFolder}',
+      }
+      dap.run(config)
+    end
+
     return {
-      { '<F1>',      dap.continue,          desc = 'Debug: Start/Continue' },
-      { '<F2>',      dap.step_into,         desc = 'Debug: Step Into' },
-      { '<F3>',      dap.step_over,         desc = 'Debug: Step Over' },
-      { '<F4>',      dap.step_out,          desc = 'Debug: Step Out' },
-      { '<F5>',      dap.step_back,         desc = 'Debug: Step Back' },
-      { '<F6>',      dap.restart,           desc = 'Debug: Restart' },
-      { '<C-\\>',    dapui.toggle,          desc = 'DAP: Check last session results' },
-      { '<leader>b', dap.toggle_breakpoint, desc = 'DAP: Toggle breakpoint' },
+      { '<A-\\>',     dap.continue,          desc = 'DAP: Start/Continue' },
+      { '<leader>dr', start_dap_with_args,   desc = 'DAP: Start with args' },
+      { '<F2>',       dap.step_into,         desc = 'DAP: Step Into' },
+      { '<F3>',       dap.step_over,         desc = 'DAP: Step Over' },
+      { '<F4>',       dap.step_out,          desc = 'DAP: Step Out' },
+      { '<F5>',       dap.step_back,         desc = 'DAP: Step Back' },
+      { '<F6>',       dap.restart,           desc = 'DAP: Restart' },
+      { '<C-\\>',     dapui.toggle,          desc = 'DAP: Check last session results' },
+      { '<leader>b',  dap.toggle_breakpoint, desc = 'DAP: Toggle breakpoint' },
       {
         '<leader>B',
         function ()
