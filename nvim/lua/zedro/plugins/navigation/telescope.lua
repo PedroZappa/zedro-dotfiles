6c93dfb1f0fa8ecee76f8f50c8a7c67d13592a0b
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-09-29 12:10:27 +0100

    nvim/*/telescope.lua : DAP bindings

--- nvim/lua/zedro/plugins/navigation/telescope.lua
+++ nvim/lua/zedro/plugins/navigation/telescope.lua
@@ -138,6 +138,7 @@ return {
     pcall(require("telescope").load_extension('harpoon'))
     pcall(require('telescope').load_extension('fzf'))
     pcall(require("telescope").load_extension("live_grep_args"))
+    pcall(require('telescope').load_extension('dap'))
 
     -- Get the text if selected or the text searched. Otherwise return '' (default behavior)
     local function getText()
@@ -194,6 +195,12 @@ return {
     vim.keymap.set('n', '<leader>ds', builtin.lsp_document_symbols, { desc = "[D]ocument [S]ymbols" })
     vim.keymap.set('n', '<leader>ws', builtin.lsp_dynamic_workspace_symbols, { desc = "[W]orkspace [S]ymbols" })
     vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = "[R]e[n]ame variable" })
+    -- DAP bindings 
+    vim.keymap.set('n', '<leader>dc', telescope.extensions.dap.commands, { desc = "[D]AP : [c]ommands" })
+    vim.keymap.set('n', '<leader>dC', telescope.extensions.dap.configurations, { desc = "[D]AP : [C]onfigurations" })
+    vim.keymap.set('n', '<leader>db', telescope.extensions.dap.list_breakpoints, { desc = "[D]AP : List [B]reakpoints" })
+    vim.keymap.set('n', '<leader>dv', telescope.extensions.dap.variables, { desc = "[D]AP : [V]ariables" })
+    vim.keymap.set('n', '<leader>df', telescope.extensions.dap.frames, { desc = "[D]AP : [F]rames" })
     -- Search grepped text
     vim.keymap.set({ 'n', 'v' }, '<leader>sg', changeText(lga), { desc = '[S]earch by [G]rep' })
     vim.keymap.set({ 'n', 'v' }, '<C-g>', changeText(lga), { desc = '[S]earch by [G]rep' })
