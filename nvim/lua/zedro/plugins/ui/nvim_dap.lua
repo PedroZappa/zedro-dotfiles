6d671888dd5e3b05cbd61836c78ee3745e1db199
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-09-29 18:53:19 +0100

    nvim/*/nvim_dap.lua : riced for codelldb

--- nvim/lua/zedro/plugins/ui/nvim_dap.lua
+++ nvim/lua/zedro/plugins/ui/nvim_dap.lua
@@ -1,26 +1,26 @@
 return {
-  'mfussenegger/nvim-dap',
+  "mfussenegger/nvim-dap",
   event = "VeryLazy",
   dependencies = {
-    'rcarriga/nvim-dap-ui',
-    'theHamsta/nvim-dap-virtual-text',
+    "rcarriga/nvim-dap-ui",
+    "theHamsta/nvim-dap-virtual-text",
     -- Dependency for ASIO
-    'nvim-neotest/nvim-nio',
+    "nvim-neotest/nvim-nio",
     -- Install debug adapters
     "williamboman/mason.nvim",
-    'jay-babu/mason-nvim-dap.nvim',
+    "jay-babu/mason-nvim-dap.nvim",
     -- Telescope integration
-    'nvim-telescope/telescope-dap.nvim',
+    "nvim-telescope/telescope-dap.nvim",
     -- Language specific
     -- 'mfussenegger/nvim-dap-python',
   },
-  config = function ()
+  config = function()
     local dap_ok, dap = pcall(require, "dap")
-    if not (dap_ok) then
+    if not dap_ok then
       print("nvim-dap not installed!")
       return
     end
-    require('dap').set_log_level('INFO') -- Helps when configuring DAP, see logs with :DapShowLog
+    require("dap").set_log_level("INFO") -- Helps when configuring DAP, see logs with :DapShowLog
 
     local ui = require("dapui")
 
@@ -33,48 +33,86 @@ return {
     })
 
     -- Adapter Configurations
-    dap.configuration = {
-      codelldb = {
-        name = 'DAP running w/ codelldb Adapter',
-        type = 'codelldb',
-        request = 'launch',
-        program = "${file}",
-        -- cwd = '${workspaceFolder}',
-        -- stopOnEntry = false
+    dap.configurations.cpp = {
+      {
+        name = "Launch",
+        type = "codelldb",
+        request = "launch",
+        program = function()
+          return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+        end,
+        cwd = "${workspaceFolder}",
+        stopAtBeginningOfMainSubprogram = true,
       },
-      bashdb = {
-        name = 'DAP running w/ bashdb Adapter',
-        type = 'bashdb',
-        request = 'launch',
+      {
+        name = "Select and attach to process",
+        type = "codelldb",
+        request = "attach",
+        program = function()
+          return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+        end,
+        pid = function()
+          local name = vim.fn.input("Executable name (filter): ")
+          return require("dap.utils").pick_process({ filter = name })
+        end,
+        cwd = "${workspaceFolder}",
+      },
+      {
+        name = "Attach to gdbserver :1234",
+        type = "codelldb",
+        request = "attach",
+        target = "localhost:1234",
+        program = function()
+          return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+        end,
+        cwd = "${workspaceFolder}",
+      },
+    }
+    dap.configurations.sh = {
+      {
+        type = "bashdb",
+        request = "launch",
+        name = "Launch file",
+        showDebugOutput = true,
+        pathBashdb = vim.fn.stdpath("data") .. "/mason/packages/bash-debug-adapter/extension/bashdb_dir/bashdb",
+        pathBashdbLib = vim.fn.stdpath("data") .. "/mason/packages/bash-debug-adapter/extension/bashdb_dir",
+        trace = true,
+        file = "${file}",
         program = "${file}",
-        cwd = '${workspaceFolder}',
-        stopOnEntry = false
-      }
+        cwd = "${workspaceFolder}",
+        pathCat = "cat",
+        pathBash = "/bin/bash",
+        pathMkfifo = "mkfifo",
+        pathPkill = "pkill",
+        args = {},
+        env = {},
+        terminalKind = "integrated",
+      },
     }
 
     -- UI : see |:help nvim-dap-ui|
     ui.setup({
-      icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+      icons = { expanded = "▾", collapsed = "▸", current_frame = "*" },
       controls = {
         icons = {
-          pause = '⏸',
-          play = '▶',
-          step_into = '⏎',
-          step_over = '⏭',
-          step_out = '⏮',
-          step_back = 'b',
-          run_last = '▶▶',
-          terminate = '⏹',
-          disconnect = '⏏',
+          pause = "⏸",
+          play = "▶",
+          step_into = "⏎",
+          step_over = "⏭",
+          step_out = "⏮",
+          step_back = "b",
+          run_last = "▶▶",
+          terminate = "⏹",
+          disconnect = "⏏",
         },
       },
     })
     require("nvim-dap-virtual-text").setup({
       -- This just tries to mitigate the chance that I leak tokens here. Probably won't stop it from happening...
-      display_callback = function (variable)
+      display_callback = function(variable)
         local name = string.lower(variable.name)
         local value = string.lower(variable.value)
-        if name:match "secret" or name:match "api" or value:match "secret" or value:match "api" then
+        if name:match("secret") or name:match("api") or value:match("secret") or value:match("api") then
           return "*****"
         end
 
@@ -86,32 +124,56 @@ return {
       end,
     })
 
+    function get_element_id_at_cursor()
+      local cursor = vim.api.nvim_win_get_cursor(0)
+      local row, col = cursor[1] - 1, cursor[2]
+      local buf = vim.api.nvim_get_current_buf()
+      local parser = vim.treesitter.get_parser(buf)
+      local tree = parser:parse()[1]
+      local root = tree:root()
+      local node = root:named_descendant_for_range(row, col, row, col)
+
+      while node do
+        if node:type() == "element_declaration" then
+          local id_node = node:child(0)
+          if id_node then
+            return vim.treesitter.get_node_text(id_node, buf)
+          end
+        end
+        node = node:parent()
+      end
+
+      return nil
+    end
+
+    -- require("dapui").float_element(get_element_id_at_cursor())
+
     -- Event Listeners
-    dap.listeners.before.attach.dapui_config = function ()
+    dap.listeners.before.attach.dapui_config = function()
       ui.open()
     end
-    dap.listeners.before.launch.dapui_config = function ()
+    dap.listeners.before.launch.dapui_config = function()
       ui.open()
     end
-    dap.listeners.before.event_terminated.dapui_config = function ()
+    dap.listeners.before.event_terminated.dapui_config = function()
       ui.close()
     end
-    dap.listeners.before.event_exited.dapui_config = function ()
+    dap.listeners.before.event_exited.dapui_config = function()
       ui.close()
     end
   end,
-  keys = function (_, keys)
-    local dap = require('dap')
-    local dapui = require('dapui')
+  keys = function(_, keys)
+    local dap = require("dap")
+    local dapui = require("dapui")
 
     -- Run DAP w/ args function
     local function start_dap_with_args()
       local filetype = vim.bo.filetype
       -- Map filetypes to debug adapter types
       local adapter_map = {
-        c = 'codelldb',
-        cpp = 'codelldb',
-        bash = 'bashdb',
+        c = "codelldb",
+        cpp = "codelldb",
+        bash = "bash-debug-adapter",
         -- Add more mappings as needed
       }
       -- Get the debug adapter type for the current filetype
@@ -121,42 +183,43 @@ return {
         return
       end
       -- Prompt user for program and arguments
-      local program = vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-      local args = vim.split(vim.fn.input('Arguments: ', vim.fn.getcwd() .. '/'), " ")
+      local program = vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+      local args = vim.split(vim.fn.input("Arguments: ", vim.fn.getcwd() .. "/"), " ")
 
       local config = {
-        type = 'codelldb', -- or the appropriate type for your debugger
-        request = 'launch',
+        type = adapter_type,
+        request = "launch",
         program = program,
         args = args,
-        cwd = '${workspaceFolder}',
+        cwd = "${workspaceFolder}",
       }
       dap.run(config)
     end
 
     return {
-      { '<A-\\>',     start_dap_with_args,   desc = 'DAP: Start with args' },
-      { '<F2>',       dap.step_into,         desc = 'DAP: Step Into' },
-      { '<F3>',       dap.step_over,         desc = 'DAP: Step Over' },
-      { '<F4>',       dap.step_out,          desc = 'DAP: Step Out' },
-      { '<F5>',       dap.step_back,         desc = 'DAP: Step Back' },
-      { '<F6>',       dap.restart,           desc = 'DAP: Restart' },
-      { '<C-\\>',     dapui.toggle,          desc = 'DAP: Check last session results' },
-      { '<leader>b',  dap.toggle_breakpoint, desc = 'DAP: Toggle breakpoint' },
+      { "<A-\\>",    start_dap_with_args,   desc = "DAP: Start with args" },
+      { "<C-`>",     dap.continue,          desc = "DAP: Continue" },
+      { "<C-s>",     dap.step_into,         desc = "DAP: Step Into" },
+      { "<C-n>",     dap.step_over,         desc = "DAP: Step Over" },
+      { "<C-B>",     dap.step_out,          desc = "DAP: Step Out" },
+      { "<C-p>",     dap.step_back,         desc = "DAP: Step Back" },
+      { "<C-r>",     dap.restart,           desc = "DAP: Restart" },
+      { "<C-\\>",    dapui.toggle,          desc = "DAP: Check last session results" },
+      { "<leader>b", dap.toggle_breakpoint, desc = "DAP: Toggle breakpoint" },
       {
-        '<leader>B',
-        function ()
-          dap.set_breakpoint(vim.fn.input('DAP: Breakpoint condition: '))
+        "<leader>B",
+        function()
+          dap.set_breakpoint(vim.fn.input("DAP: Breakpoint condition: "))
         end,
-        { desc = 'DAP: Set breakpoint' }
+        { desc = "DAP: Set breakpoint" },
       },
-      { '<leader>gb', dap.run_to_cursor, desc = 'DAP: Run to cursor' },
+      { "<leader>gb", dap.run_to_cursor, desc = "DAP: Run to cursor" },
       {
-        '<leader>?',
-        function ()
+        "<leader>?",
+        function()
           require("dapui").eval(nil, { enter = true })
         end,
-        { desc = 'DAP: Evaluate expression under cursor' }
+        { desc = "DAP: Evaluate expression under cursor" },
       },
       unpack(keys),
     }
