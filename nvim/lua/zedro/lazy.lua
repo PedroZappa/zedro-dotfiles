c033cd68fddfdc45edcaaccb0027f810608ce65d
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-10-25 15:20:33 +0100

    nvim : cleanup

--- nvim/lua/zedro/lazy.lua
+++ nvim/lua/zedro/lazy.lua
@@ -1,18 +1,3 @@
--- -- Lazy (Package Manager)
--- -- vim.loader.enable() -- cache lua modules (https://github.com/neovim/neovim/pull/22668)
--- local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
--- if not vim.loop.fs_stat(lazypath) then
---   vim.fn.system({
---     "git",
---     "clone",
---     "--filter=blob:none",
---     "https://github.com/folke/lazy.nvim.git",
---     "--branch=stable", -- latest stable release
---     lazypath,
---   })
--- end
--- vim.opt.rtp:prepend(lazypath)
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
