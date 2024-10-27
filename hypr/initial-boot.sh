9c87a1c236f6b08b3ec5603756681a8f255e7382
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-10-27 20:18:18 +0000

    hypr & some 󰟪

--- hypr/initial-boot.sh
+++ hypr/initial-boot.sh
@@ -11,7 +11,7 @@ wallpaper=$HOME/.config/hypr/wallpaper_effects/.wallpaper_modified
 waybar_style="$HOME/.config/waybar/style/[Colored] Chroma Glow.css"
 kvantum_theme="Catppuccin-Mocha"
 color_scheme="prefer-dark"
-gtk_theme="Andromeda-dark"
+gtk_theme="Yaru-magenta-dark"
 icon_theme="Flat-Remix-Blue-Dark"
 cursor_theme="Bibata-Modern-Ice"
 
