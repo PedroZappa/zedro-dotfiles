6137f13378369144f44f79b68fbee21a0f6d13ed
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-11-23 08:25:36 +0000

    image rendring on markdown

--- nvim/lua/zedro/plugins/image.lua
+++ nvim/lua/zedro/plugins/image.lua
@@ -1,22 +1,45 @@
 return {
-  -- "samodostal/image.nvim",
-  -- requires = { "nvim-lua/plenary.nvim" },
-  -- dependencies = {
-  --   { "m00qek/baleia.nvim", tag = "v1.3.0" }, -- color support
-  -- },
-  -- config = function()
-  --   require("image").setup({
-  --     render = {
-  --       min_padding = 5,
-  --       show_label = true,
-  --       show_image_dimensions = true,
-  --       use_dither = true,
-  --       foreground_color = true,
-  --       background_color = true,
-  --     },
-  --     events = {
-  --       update_on_nvim_resize = true,
-  --     },
-  --   })
-  -- end,
+  "samodostal/image.nvim",
+  requires = { "nvim-lua/plenary.nvim" },
+  dependencies = {
+    { "m00qek/baleia.nvim", tag = "v1.3.0" }, -- color support
+  },
+  config = function()
+    require("image").setup({
+      backend = "kitty",
+      processor = "magick_cli", -- or "magick_rock"
+      integrations = {
+        markdown = {
+          enabled = true,
+          clear_in_insert_mode = false,
+          download_remote_images = true,
+          only_render_image_at_cursor = false,
+          filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+        },
+        neorg = {
+          enabled = true,
+          filetypes = { "norg" },
+        },
+        typst = {
+          enabled = true,
+          filetypes = { "typst" },
+        },
+        html = {
+          enabled = false,
+        },
+        css = {
+          enabled = false,
+        },
+      },
+      max_width = nil,
+      max_height = nil,
+      max_width_window_percentage = nil,
+      max_height_window_percentage = 50,
+      window_overlap_clear_enabled = false,                                           -- toggles images when windows are overlapped
+      window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+      editor_only_render_when_focused = false,                                        -- auto show/hide images when the editor gains/looses focus
+      tmux_show_only_in_active_window = false,                                        -- auto show/hide images in the correct Tmux window (needs visual-activity off)
+      hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" }, -- render image files as images when opened
+    })
+  end,
 }
