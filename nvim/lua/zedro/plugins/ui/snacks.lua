52b9ef7ae6e5dc59cb33b78e70f0d403671b13db
Zedro <45104292+PedroZappa@users.noreply.github.com>
2025-02-14 14:31:28 +0000

    nvim : snacks bigfile

--- nvim/lua/zedro/plugins/ui/snacks.lua
+++ nvim/lua/zedro/plugins/ui/snacks.lua
@@ -360,7 +360,26 @@ return {
       easing = "linear",
       fps = 60, -- frames per second. Global setting for all animations
     },
-    bigfile = { enabled = true },
+    bigfile = {
+      enabled = true,
+      notify = true, -- show notification when big file detected
+      size = (1.5 * 1024 * 1024), -- 1.5MB
+      line_length = 1000, -- average line length (useful for minified files)
+      -- Enable or disable features when big file detected
+      ---@param ctx {buf: number, ft:string}
+      setup = function(ctx)
+        if vim.fn.exists(":NoMatchParen") ~= 0 then
+          vim.cmd([[NoMatchParen]])
+        end
+        Snacks.util.wo(0, { foldmethod = "manual", statuscolumn = "", conceallevel = 0 })
+        vim.b.minianimate_disable = true
+        vim.schedule(function()
+          if vim.api.nvim_buf_is_valid(ctx.buf) then
+            vim.bo[ctx.buf].syntax = ctx.ft
+          end
+        end)
+      end,
+    },
     dashboard = { enabled = true },
     explorer = { enabled = true },
     indent = { enabled = true },
