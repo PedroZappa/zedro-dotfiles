a87f67e2d23f9d5499cb0b598552ec2a1c5d0dd1
Zedro <45104292+PedroZappa@users.noreply.github.com>
2025-02-14 15:05:50 +0000

    nvim : snacks image

--- nvim/lua/zedro/plugins/ui/snacks.lua
+++ nvim/lua/zedro/plugins/ui/snacks.lua
@@ -516,6 +516,47 @@ return {
         },
       },
     },
+    health = { enabled = true },
+    image = {
+      formats = {
+        "png",
+        "jpg",
+        "jpeg",
+        "gif",
+        "bmp",
+        "webp",
+        "tiff",
+        "heic",
+        "avif",
+        "mp4",
+        "mov",
+        "avi",
+        "mkv",
+        "webm",
+      },
+      force = false, -- try displaying the image, even if the terminal does not support it
+      markdown = {
+        -- enable image viewer for markdown files
+        -- if your env doesn't support unicode placeholders, this will be disabled
+        enabled = true,
+        max_width = 80,
+        max_height = 40,
+      },
+      -- window options applied to windows displaying image buffers
+      -- an image buffer is a buffer with `filetype=image`
+      wo = {
+        wrap = false,
+        number = false,
+        relativenumber = false,
+        cursorcolumn = false,
+        signcolumn = "no",
+        foldcolumn = "0",
+        list = false,
+        spell = false,
+        statuscolumn = "",
+      },
+      env = {},
+    },
     indent = { enabled = true },
     input = { enabled = true },
     notifier = {
